04406339680cce7c769040c7e5b92c0a51d92750
alvinng4 <alvinng324@gmail.com>
2024-11-06 23:55:07 +0800

    Initial commit

---
+++ mcts.c
@@ -0,0 +1,513 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "common.h"
+#include "mcts.h"
+
+int8 initialize_node_pool(Node **node_pool, int32 total_num_nodes)
+{
+    // printf("Amount of memory to allocate in GB: %f\n", total_num_nodes * sizeof(Node) / 1024.0 / 1024.0 / 1024.0);
+    *node_pool = malloc(total_num_nodes * sizeof(Node));
+    if (*node_pool == NULL)
+    {
+        fprintf(stderr, "Error: Unable to allocate memory for node pool in initialize_node_pool()\n");
+        return 1;
+    }
+    return 0;
+}
+
+void free_node_pool(Node *restrict node_pool)
+{
+    free(node_pool);
+}
+
+void initialize_root(
+    Node *root,
+    float *restrict encoded_white_board,
+    float *restrict encoded_black_board,
+    int8 *restrict white_board,
+    bool *restrict board_metadata,
+    int8 en_passant,
+    int8 num_half_moves,
+    float *restrict policy,
+    float w,
+    float d,
+    float l
+)
+{
+    root->parent = NULL;
+    root->wdl[0] = w;
+    root->wdl[1] = d;
+    root->wdl[2] = l;
+
+    memcpy(root->encoded_white_board, encoded_white_board, 112 * 64 * sizeof(float));
+    memcpy(root->encoded_black_board, encoded_black_board, 112 * 64 * sizeof(float));
+    memcpy(root->white_board, white_board, 64 * sizeof(int8));
+    memcpy(root->board_metadata, board_metadata, 6 * sizeof(bool));
+    root->en_passant = en_passant;
+    root->num_half_moves = num_half_moves;
+
+    for (int16 i = 0; i < 1792; i++)
+    {
+        if (policy[i] != 0.0)
+        {
+            if (board_metadata[5])
+            {
+                root->legal_from[root->num_legal_actions] = white_policy_idx_to_moves_from[i];
+                root->legal_to[root->num_legal_actions] = white_policy_idx_to_moves_to[i];
+            }
+            else
+            {
+                root->legal_from[root->num_legal_actions] = black_policy_idx_to_moves_from[i];
+                root->legal_to[root->num_legal_actions] = black_policy_idx_to_moves_to[i];
+            }
+
+            /* Check if it is promoting to knight */
+            bool is_promotion_to_knight = false;
+            for (int16 j = 0; j < 22; j++)
+            {
+                if (i == possible_knight_promotion_idx[j])
+                {
+                    if (board_metadata[5] && white_board[root->legal_from[root->num_legal_actions]] == PIECE_WHITE_PAWN)
+                    {
+                        root->legal_promotion[root->num_legal_actions] = 4;
+                        is_promotion_to_knight = true;
+                        break;
+                    }
+                    else if (!board_metadata[5] && white_board[root->legal_from[root->num_legal_actions]] == PIECE_BLACK_PAWN)
+                    {
+                        root->legal_promotion[root->num_legal_actions] = 4;
+                        is_promotion_to_knight = true;
+                        break;
+                    }
+                }
+            }
+            if (!is_promotion_to_knight)
+            {
+                root->legal_promotion[root->num_legal_actions] = 0;
+            }
+            
+            root->legal_actions_prior[root->num_legal_actions] = policy[i];
+            root->num_legal_actions++;
+            if (root->num_legal_actions >= MCTS_MAX_CHILDREN)
+            {
+                fprintf(stderr, "Error: Legal actions larger than MCTS_MAX_CHILDREN in initialize_root()\n");
+                return;
+            }
+        }
+    }
+    for (int16 i = 1792; i < 1858; i++)
+    {
+        if (policy[i] != 0.0)
+        {
+            if (board_metadata[5])
+            {
+                root->legal_from[root->num_legal_actions] = white_policy_idx_to_moves_from[i];
+                root->legal_to[root->num_legal_actions] = white_policy_idx_to_moves_to[i];
+            }
+            else
+            {
+                root->legal_from[root->num_legal_actions] = black_policy_idx_to_moves_from[i];
+                root->legal_to[root->num_legal_actions] = black_policy_idx_to_moves_to[i];
+            }
+            root->legal_promotion[root->num_legal_actions] = ((i - 1792) % 3) + 1;
+        }
+        if (root->num_legal_actions >= MCTS_MAX_CHILDREN)
+        {
+            fprintf(stderr, "Error: Legal actions larger than MCTS_MAX_CHILDREN in initialize_root()\n");
+            return;
+        }
+    }
+
+    for (int16 i = 0; i < MCTS_MAX_CHILDREN; i++)
+    {
+        root->children[i] = NULL;
+    }
+
+    root->num_children = 0;
+    root->num_descents = 1;
+
+    root->num_descents_virtual_loss = 0.0;
+    root->value_virtual_loss = 0.0;
+}
+
+void reset_virtual_loss(Node *restrict node_pool, int32 num_nodes)
+{
+    for (int32 i = 0; i < num_nodes; i++)
+    {
+        node_pool[i].num_descents_virtual_loss = 0.0;
+        // node_pool[i].value_virtual_loss = 0.0;
+    }
+}
+
+void get_batch(
+    Node *node_pool,
+    int32 batch_size,
+    int32 batch_early_stop,
+    float *restrict batched_encoded_boards,
+    Node **restrict batch_nodes,
+    int32 *restrict num_batched_nodes,
+    int32 *restrict num_current_nodes,
+    int32 num_total_nodes,
+    float c_puct,
+    float c_fpu,
+    float virtual_loss
+)
+{
+    int32 current_batch_idx = 0;
+    for (int32 i = 0; (i < batch_early_stop) && (current_batch_idx < batch_size) && (*num_current_nodes < num_total_nodes); i++)
+    {
+        bool is_new_node = batch_PUCT(node_pool, &(batch_nodes[current_batch_idx]), *num_current_nodes, c_puct, c_fpu, virtual_loss);
+        if (is_new_node)
+        {
+            if (batch_nodes[current_batch_idx]->board_metadata[5])
+            {
+                memcpy(batched_encoded_boards + current_batch_idx * 64 * 112, batch_nodes[current_batch_idx]->encoded_white_board, 64 * 112 * sizeof(float));
+            }
+            else
+            {
+                memcpy(batched_encoded_boards + current_batch_idx * 64 * 112, batch_nodes[current_batch_idx]->encoded_black_board, 64 * 112 * sizeof(float));
+            }
+            current_batch_idx++;
+            *num_current_nodes += 1;
+        }
+    }
+
+    *num_batched_nodes = current_batch_idx;
+}
+
+void put_batch(
+    Node **restrict batch_nodes,
+    int32 num_batched_nodes,
+    float *restrict policy,
+    float *restrict wdl
+)
+{
+    for (int32 i = 0; i < num_batched_nodes; i++)
+    {
+        batch_nodes[i]->num_legal_actions = 0;
+        for (int16 j = 0; j < 1792; j++)
+        {
+            if (policy[i * 1858 + j] != 0.0)
+            {
+                if (batch_nodes[i]->board_metadata[5])
+                {
+                    batch_nodes[i]->legal_from[batch_nodes[i]->num_legal_actions] = white_policy_idx_to_moves_from[j];
+                    batch_nodes[i]->legal_to[batch_nodes[i]->num_legal_actions] = white_policy_idx_to_moves_to[j];
+                }
+                else
+                {
+                    batch_nodes[i]->legal_from[batch_nodes[i]->num_legal_actions] = black_policy_idx_to_moves_from[j];
+                    batch_nodes[i]->legal_to[batch_nodes[i]->num_legal_actions] = black_policy_idx_to_moves_to[j];
+                }
+
+                /* Check if it is promoting to knight */
+                bool is_promotion_to_knight = false;
+                for (int16 k = 0; k < 22; k++)
+                {
+                    if (j == possible_knight_promotion_idx[k])
+                    {
+                        if (batch_nodes[i]->board_metadata[5] && batch_nodes[i]->white_board[batch_nodes[i]->legal_from[batch_nodes[i]->num_legal_actions]] == PIECE_WHITE_PAWN)
+                        {
+                            batch_nodes[i]->legal_promotion[batch_nodes[i]->num_legal_actions] = 4;
+                            is_promotion_to_knight = true;
+                            break;
+                        }
+                        else if (!batch_nodes[i]->board_metadata[5] && batch_nodes[i]->white_board[batch_nodes[i]->legal_from[batch_nodes[i]->num_legal_actions]] == PIECE_BLACK_PAWN)
+                        {
+                            batch_nodes[i]->legal_promotion[batch_nodes[i]->num_legal_actions] = 4;
+                            is_promotion_to_knight = true;
+                            break;
+                        }
+                    }
+                }
+                if (!is_promotion_to_knight)
+                {
+                    batch_nodes[i]->legal_promotion[batch_nodes[i]->num_legal_actions] = 0;
+                }
+                
+                batch_nodes[i]->legal_actions_prior[batch_nodes[i]->num_legal_actions] = policy[i * 1858 + j];
+                batch_nodes[i]->num_legal_actions++;
+            }
+        }
+        for (int16 j = 1792; j < 1858; j++)
+        {
+            if (policy[i * 1858 + j] != 0.0)
+            {
+                if (batch_nodes[i]->board_metadata[5])
+                {
+                    batch_nodes[i]->legal_from[batch_nodes[i]->num_legal_actions] = white_policy_idx_to_moves_from[j];
+                    batch_nodes[i]->legal_to[batch_nodes[i]->num_legal_actions] = white_policy_idx_to_moves_to[j];
+                }
+                else
+                {
+                    batch_nodes[i]->legal_from[batch_nodes[i]->num_legal_actions] = black_policy_idx_to_moves_from[j];
+                    batch_nodes[i]->legal_to[batch_nodes[i]->num_legal_actions] = black_policy_idx_to_moves_to[j];
+                }
+                batch_nodes[i]->legal_promotion[batch_nodes[i]->num_legal_actions] = ((j - 1792) % 3) + 1;
+            }
+        }
+
+        back_propagate(batch_nodes[i], wdl[i * 3 + 0], wdl[i * 3 + 1], wdl[i * 3 + 2]);
+    }
+}
+
+void back_propagate_virtual_loss(Node *restrict node, float virtual_loss)
+{
+    while (node != NULL)
+    {
+        node->num_descents_virtual_loss += virtual_loss;
+        node = node->parent;
+    }
+}
+
+void back_propagate(Node *restrict node, float w, float d, float l)
+{
+    while (node != NULL)
+    {
+        node->wdl[0] += w;
+        node->wdl[1] += d;
+        node->wdl[2] += l;
+        node->num_descents += 1;
+        node = node->parent;
+
+        float temp = w;
+        w = l;
+        l = temp;
+    }
+}
+
+bool batch_PUCT(
+    Node *node_pool,
+    Node **batch_node_ptr,
+    int32 num_current_nodes,
+    float c_puct,
+    float c_fpu,
+    float virtual_loss
+)
+{
+    bool is_new_node = false;
+    Node *current_node = &(node_pool[0]);
+
+    while (true)
+    {
+        /* PUCT search */
+
+        // If no legal actions, it means that it is a leaf node (Either checkmate or stalemate)
+        if (current_node->num_legal_actions == 0)
+        {
+            if (current_node->wdl[1] > 0.0)
+            {
+                back_propagate(current_node, 0.0, 1.0, 0.0);
+            }
+            else
+            {
+                back_propagate(current_node, 0.0, 0.0, 1.0);
+            }
+
+            return false;
+        }
+        
+        float best_puct = -INFINITY;
+        int16 best_child = 0;
+        for (int16 i = 0; i < current_node->num_legal_actions; i++)
+        {
+            float puct;
+            /* Node not explored */
+            if (current_node->children[i] == NULL)
+            {
+                float v_c = (current_node->wdl[0] - current_node->wdl[2] + current_node->value_virtual_loss) / current_node->num_descents - c_fpu * sqrtf(current_node->total_policy_explored_children);
+                puct = v_c + c_puct * current_node->legal_actions_prior[i] * sqrtf(current_node->num_descents);
+            }
+
+            /* Node not explored but already inside the batch */
+            else if (current_node->children[i]->num_legal_actions == -1)
+            {
+                float v_c = (current_node->wdl[0] - current_node->wdl[2] + current_node->value_virtual_loss) / current_node->num_descents - c_fpu * sqrtf(current_node->total_policy_explored_children);
+                puct = v_c + c_puct * current_node->legal_actions_prior[i] * sqrtf(current_node->num_descents) / (1 + current_node->children[i]->num_descents_virtual_loss);
+            }
+
+            /* Explored nodes */
+            else
+            {
+                float v_c = (current_node->children[i]->wdl[2] - current_node->children[i]->wdl[0] + current_node->children[i]->value_virtual_loss) / current_node->children[i]->num_descents;
+                puct = v_c + c_puct * current_node->legal_actions_prior[i] * sqrtf(current_node->num_descents) / (1 + current_node->children[i]->num_descents + current_node->children[i]->num_descents_virtual_loss);
+            }
+
+            if (puct > best_puct)
+            {
+                best_puct = puct;
+                best_child = i;
+            }
+        }
+
+        /* Expand */
+        if (current_node->children[best_child] == NULL)
+        {
+            is_new_node = true;
+
+            Node *new_node = &(node_pool[num_current_nodes]);
+            new_node->parent = current_node;
+            new_node->wdl[0] = 0.0;
+            new_node->wdl[1] = 0.0;
+            new_node->wdl[2] = 0.0;
+
+            memcpy(new_node->white_board, current_node->white_board, 64 * sizeof(int8));
+            memcpy(new_node->board_metadata, current_node->board_metadata, 6 * sizeof(bool));
+            new_node->num_half_moves = current_node->num_half_moves;
+            
+            make_move(
+                new_node->white_board,
+                new_node->board_metadata,
+                &(new_node->en_passant),
+                &(new_node->num_half_moves),
+                current_node->legal_from[best_child],
+                current_node->legal_to[best_child],
+                current_node->legal_promotion[best_child]
+            );
+
+            encode_boards(
+                current_node->encoded_white_board,
+                current_node->encoded_black_board,
+                new_node->encoded_white_board,
+                new_node->encoded_black_board,
+                new_node->white_board,
+                new_node->board_metadata,
+                new_node->num_half_moves
+            );
+
+            new_node->num_legal_actions = -1;
+
+            new_node->total_policy_explored_children = 0.0;
+            new_node->num_children = 0;
+            for (int16 j = 0; j < MCTS_MAX_CHILDREN; j++)
+            {
+                new_node->children[j] = NULL;
+            }
+
+            new_node->num_descents = 0;
+            new_node->num_descents_virtual_loss = 0.0;
+            new_node->value_virtual_loss = 0.0;
+
+            current_node->children[best_child] = new_node;
+            current_node->children_idx[current_node->num_children] = best_child;
+            current_node->num_children += 1;
+            current_node->total_policy_explored_children += current_node->legal_actions_prior[best_child];
+
+            current_node = new_node;
+        }
+        else
+        {
+            current_node = current_node->children[best_child];
+
+            /* Node already in the batch */
+            if (current_node->num_legal_actions == -1)
+            {
+                back_propagate_virtual_loss(current_node, virtual_loss);
+                return false;
+            }
+
+            is_new_node = false;
+        }
+
+        if (is_new_node)
+        {
+            int8 terminal_check = is_terminal(
+                current_node->white_board, current_node->board_metadata, current_node->en_passant
+            );
+
+            /* Stalemate */
+            if (terminal_check == 0)
+            {
+                current_node->num_legal_actions = 0;
+                back_propagate(current_node, 0.0, 1.0, 0.0);
+                return false;
+            }
+            /* Checkmate */
+            else if (terminal_check == 1)
+            {
+                current_node->num_legal_actions = 0;
+                back_propagate(current_node, 0.0, 0.0, 1.0);
+                return false;
+            }    
+                
+            /* node need to be evaluated by NN */
+            else
+            {
+                *batch_node_ptr = current_node;
+                back_propagate_virtual_loss(current_node, virtual_loss);
+                return true;
+            }
+        }
+    }
+}
+
+void mcts_batches_mask_illegal_and_softmax_policy(
+    float *restrict policy,
+    Node **batch_nodes,
+    int32 num_batched_nodes
+)
+{
+    for (int32 i = 0; i < num_batched_nodes; i++)
+    {
+        mask_illegal_and_softmax_policy(
+            policy + i * 1858,
+            batch_nodes[i]->white_board,
+            batch_nodes[i]->board_metadata,
+            batch_nodes[i]->en_passant
+        );
+    }
+}
+
+void get_best_move_and_wdl(
+    Node *root,
+    int8 *restrict from,
+    int8 *restrict to,
+    int8 *restrict promotion,
+    float *restrict w,
+    float *restrict d,
+    float *restrict l
+)
+{
+    if (root->num_children == 0)
+    {
+        *from = root->legal_from[0];
+        *to = root->legal_to[0];
+        *promotion = root->legal_promotion[0];
+        *w = 0.0;
+        *d = 0.0;
+        *l = 0.0;
+        return;
+    }
+    
+    int32 most_visits = 0;
+    int16 best_child = 0;
+    for (int16 i = 0; i < root->num_children; i++)
+    {
+        if (root->children[root->children_idx[i]]->num_descents > most_visits)
+        {
+            most_visits = root->children[root->children_idx[i]]->num_descents;
+            best_child = root->children_idx[i];
+        }
+        printf("Prior: %f, Num_visits: %d, value: %f, from: %d, to: %d\n",
+            root->legal_actions_prior[root->children_idx[i]], 
+            root->children[root->children_idx[i]]->num_descents, 
+            (
+                root->children[root->children_idx[i]]->wdl[2] + 0.5 * root->children[root->children_idx[i]]->wdl[1]
+            ) / root->children[root->children_idx[i]]->num_descents,
+            root->legal_from[root->children_idx[i]],
+            root->legal_to[root->children_idx[i]]
+        );
+    }
+
+    *from = root->legal_from[best_child];
+    *to = root->legal_to[best_child];
+    *promotion = root->legal_promotion[best_child];
+
+    // Win of children = loss of parent
+    *l = root->children[best_child]->wdl[0] / root->children[best_child]->num_descents;
+    *d = root->children[best_child]->wdl[1] / root->children[best_child]->num_descents;
+    *w = root->children[best_child]->wdl[2] / root->children[best_child]->num_descents;
+}
---
+++ uci.py
@@ -0,0 +1,152 @@
+import yaml
+from pathlib import Path
+
+import torch
+
+import utils
+from chess_lib import Chess
+
+RANDOM_STATE = 1
+utils.seed_everything(RANDOM_STATE)
+
+def main():
+    print("ChessSL version 0.0.1")
+    c_lib = utils.load_c_lib()
+    
+    if not (Path(__file__).parent / "config.yaml").exists():
+        raise FileNotFoundError("config.yaml not found.")
+
+    with open(Path(__file__).parent / "config.yaml") as file:
+        engine_config = yaml.safe_load(file)["engine"]
+
+    model = torch.load(engine_config["model_path"], map_location=torch.device(engine_config["device"]), weights_only=False)
+    model.eval()
+
+    chess = Chess(
+        c_lib, 
+        model, 
+        engine_config["device"], 
+        engine_config["mcts_batch_size"],
+        engine_config["c_puct"],
+        engine_config["c_fpu"],
+        engine_config["virtual_loss"],
+    )
+    
+    show_wdl = False
+    while True:
+        user_input = input().strip().split()
+        user_input_len = len(user_input)
+        if user_input_len > 0:
+            match user_input[0]:
+                case "uci":
+                    print("id name ChessSL")
+                    print("id author Ching-Yin Ng")
+                    print()
+                    print("option name UCI_ShowWDL type check default false")
+                    print("uciok")
+                case "isready":
+                    print("readyok")
+                case "ucinewgame":
+                    chess.new_game()
+                case "position":
+                    if user_input[1] == "startpos":
+                        if user_input_len == 2:
+                            chess.new_game()
+                        elif user_input_len > 3:
+                            if (user_input_len > 3 + len(chess.moves)) and ((user_input[3:(3 + len(chess.moves) + 1)] == chess.moves)):
+                                chess.make_moves(
+                                    c_lib,
+                                    chess.white_board,
+                                    chess.encoded_white_board,
+                                    chess.encoded_black_board,
+                                    chess.board_metadata,
+                                    chess.en_passant_ctypes,
+                                    chess.num_half_moves_ctypes,
+                                    chess.moves,
+                                    user_input[3:],
+                                )
+                            else:
+                                chess.new_game(moves=user_input[3:])
+                    elif user_input[1] == "fen":
+                        if user_input_len == 8:
+                            chess.new_game(fen_string=user_input[2:])
+                        elif user_input_len > 9:
+                            if (user_input_len > 9 + len(chess.moves)) and ((user_input[9:(9+len(chess.moves))] == chess.moves[len(chess.moves)])):
+                                chess.make_moves(
+                                    c_lib,
+                                    chess.white_board,
+                                    chess.encoded_white_board,
+                                    chess.encoded_black_board,
+                                    chess.board_metadata,
+                                    chess.en_passant_ctypes,
+                                    chess.num_half_moves_ctypes,
+                                    chess.moves,
+                                    user_input[9:],
+                                )
+                            else:
+                                chess.new_game(
+                                    fen_string=user_input[2:8],
+                                    moves=user_input[9:]
+                                )
+                case "go":
+                    best_move, wdl = chess.search(
+                        chess.c_lib,
+                        chess.model,
+                        chess.device,
+                        chess.white_board,
+                        chess.encoded_white_board,
+                        chess.encoded_black_board,
+                        chess.board_metadata,
+                        chess.en_passant_ctypes,
+                        chess.num_half_moves_ctypes,
+                        engine_config["default_num_nodes"],
+                        chess.batched_encoded_boards,
+                        chess.batch_size,
+                        engine_config["mcts_batches_early_stop"],
+                        chess.c_puct,
+                        chess.c_fpu,
+                        chess.virtual_loss,
+                    )          
+                    chess.make_moves(
+                        c_lib,
+                        chess.white_board,
+                        chess.encoded_white_board,
+                        chess.encoded_black_board,
+                        chess.board_metadata,
+                        chess.en_passant_ctypes,
+                        chess.num_half_moves_ctypes,
+                        chess.moves,
+                        chess.moves + [best_move]
+                    )
+                    if show_wdl:
+                        print(f"info wdl {wdl[0] * 1000.0 :.0f} {wdl[1] * 1000.0:.0f} {wdl[2] * 1000.0:.0f}")
+                    print(f"bestmove {best_move}")
+                case "setoption":
+                    if len(user_input) >= 5 and user_input[1] == "name" and user_input[2] == "UCI_ShowWDL":
+                        if (user_input[4] == "true"):
+                            show_wdl = True
+                        elif (user_input[4] == "false"):
+                            show_wdl = False
+                case "quit":
+                    break
+                case "debug":
+                    chess.debug_print_board()
+                case _:
+                    print(f"Unknown command {user_input[0]}")
+
+        
+
+
+if __name__ == "__main__":
+    main()
+    
+
+
+
+
+
+
+
+
+
+
